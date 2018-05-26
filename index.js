1986b2da66453a3843e5017e2d2ed970220295d4
Hanbi Hans Choi <personabe1984@gmail.com>
2018-05-26 22:42:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);  
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested');
+}
+
+function deepestChild(){
+  const deepest = document.getElementById('app');
+  return deepest;
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  for(let i = 0; i < lis.length; i++){
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+  }
+}
+
+
+
+console.log(nestedTarget());
